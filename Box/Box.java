575eef2b380458de2f6791b5ed9f96f324636be3
MariaMarz <126455312+MariaMarz@users.noreply.github.com>
2023-03-21 11:30:32 +0100

    Add files via upload

---
+++ Box/Box.java
@@ -0,0 +1,53 @@
+package Homework.Box;
+
+public class Box {
+    public static void main(String[] args) {
+
+        int wight;
+        int height;
+        int depth;
+
+        BoxOne box1 = new BoxOne(12, 6, 4);
+        System.out.println();
+
+        BoxOne box2 = new BoxOne(10, 4);
+        System.out.println();
+
+        BoxOne box3 = new BoxOne(8);
+        System.out.println();
+
+        BoxOne box4 = new BoxOne();
+        System.out.println();
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
