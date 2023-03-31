9564192da372b3d84731a06f2187b2d614206223
MariaMarz <126455312+MariaMarz@users.noreply.github.com>
2023-03-31 10:51:25 +0200

    Update KolbaTemperature.java

--- KolbaTemperature.java
+++ KolbaTemperature.java
@@ -16,3 +16,23 @@ public class KolbaTemperature {
         }
     }
 
+но думаю правильно с условным оператором, переделала вот:
+
+package Homework;
+
+public class ColbaTemperature1 {
+    public static void main(String[] args) {
+        int Temperature1 = 120;
+        int Temperature2 = 70;
+        boolean q = (Temperature1 > 100) && (Temperature2 < 100);
+        System.out.println(q);
+
+        if ((Temperature1 > 100) && (Temperature2 < 100)) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+
+        }
+
+    }
+}
